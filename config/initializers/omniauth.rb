902b914f9b283cc7c0d671c142110a06fe72b441
Dmytro Kazak <dmytrokazak@gmail.com>
2016-09-02 18:59:58 +0300

    google auth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :google_oauth2, '296473228932-2stolrcmus6rlv2efi3218umpr026cmq.apps.googleusercontent.com', 'kAFGvMEsCBFINL_QFOq0bi-I', {client_options: {ssl: {:ca_file => "#{Rails.root}/lib/ca-bundle.crt"}}}
+end
