d56c215c1520229bc024597fc72a20762bef603b
Dmytro Kazak <dmytrokazak@gmail.com>
2016-09-03 13:36:26 +0300

    working playlist method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,42 @@
+require 'rest-client'
+require 'json'
+
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	helper_method :current_user
-	helper_method :display_playlist
+	helper_method :get_playlists
 	
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
 	
-	def display_playlist
+	# https://developers.google.com/youtube/v3/docs/playlists/list#response
+	def get_playlists
+		res = []
 		
-		playlists = Playlists.get_user_playlists(current_user)
-		puts playlists
-	
+		if current_user && current_user.oauth_token.length > 0
+			RestClient.get(
+				"https://www.googleapis.com/youtube/v3/playlists",
+				:params => {
+					:part         => "snippet",
+					:mine         => true,
+					:key          => "AIzaSyBfjsc4qFp_BkhjZ9PQgbxTwfzRAeUvmoM",
+					:access_token => current_user.oauth_token
+				}
+			){ |response, request, result, &block|
+				case response.code
+					when 200
+						playlists_info = JSON.parse(response.to_str)["items"]
+						playlists_info.each { |entry| res << entry["snippet"]["localized"]["title"].html_safe }
+					else
+						res << "Result: #{result}".html_safe
+						res << "Response: #{response.to_str}".html_safe
+						res << "Request: #{request}".html_safe
+						res << "Headers: #{response.raw_headers}".html_safe
+				end
+			}
+		end
+		
+		res
 	end
-	
-	def self.print_user
-	
-	end
-
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,14 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :google_oauth2, '296473228932-2stolrcmus6rlv2efi3218umpr026cmq.apps.googleusercontent.com', 'kAFGvMEsCBFINL_QFOq0bi-I', {client_options: {ssl: {:ca_file => "#{Rails.root}/lib/ca-bundle.crt"}}}
+	provider :google_oauth2, '296473228932-2stolrcmus6rlv2efi3218umpr026cmq.apps.googleusercontent.com', 'kAFGvMEsCBFINL_QFOq0bi-I', {
+				 :client_options => {
+					 :ssl => {
+						 :ca_file => "#{Rails.root}/lib/ca-bundle.crt"
+					 }
+				 },
+				 
+				 :scope => "email, profile, https://www.googleapis.com/auth/youtube",
+				 :prompt => "consent"
+			 }
 end
