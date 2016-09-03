87e1592e208df0dd27f995b505d54ac662d3eda3
Dmytro Kazak <dmytrokazak@gmail.com>
2016-09-03 14:12:33 +0300

    scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,13 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :google_oauth2, '296473228932-2stolrcmus6rlv2efi3218umpr026cmq.apps.googleusercontent.com', 'kAFGvMEsCBFINL_QFOq0bi-I', {
-				 :client_options => {
-					 :ssl => {
-						 :ca_file => "#{Rails.root}/lib/ca-bundle.crt"
-					 }
-				 },
-				 
-				 :scope => "email, profile, https://www.googleapis.com/auth/youtube",
-				 :prompt => "consent"
-			 }
+	provider :google_oauth2, "296473228932-2stolrcmus6rlv2efi3218umpr026cmq.apps.googleusercontent.com", "kAFGvMEsCBFINL_QFOq0bi-I", {
+		:client_options => {
+			:ssl => {
+				:ca_file => "#{Rails.root}/lib/ca-bundle.crt"
+			}
+		},
+		:scope          => "https://www.googleapis.com/auth/userinfo.email, https://www.googleapis.com/auth/userinfo.profile, https://www.googleapis.com/auth/youtube, https://www.googleapis.com/auth/youtube.readonly",
+		:prompt 		=> "select_account consent",
+	}
 end
